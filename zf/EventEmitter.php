3f56b707750e7c432ac6eac0807b248a5196859d
Feng Zhou <zf.pascal@gmail.com>
2013-05-10 21:47:57 +0800

    make event bubble

--- zf/EventEmitter.php
+++ zf/EventEmitter.php
@@ -5,20 +5,30 @@ namespace zf;
 trait EventEmitter
 {
 	private $listeners;
+	private $parent;
 
 	public function on($event, $callback)
 	{
 		$this->listeners[$event][] = $callback;
+		return $this;
 	}
 
 	public function emit($event, $data)
 	{
-		if(is_array($this->listeners[$event]))
+		if(isset($this->listeners[$event]) && is_array($this->listeners[$event]))
 		{
 			foreach($this->listeners[$event] as $callback)
 			{
-				if($this->callClosure('events', $callback, $this, [$data])) break;
+				if($this->callClosure('events', $callback, $this, [$data])) return $this;
 			}
 		}
+		is_null($this->parent) or $this->parent->emit($event, $data);
+		return $this;
+	}
+
+	public function setParent($parent)
+	{
+		$this->parent = $parent;
+		return $this;
 	}
 }
