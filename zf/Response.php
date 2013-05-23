bf5c7257f772b630323c19708ff442e491091ede
Feng Zhou <zf.pascal@gmail.com>
2013-05-23 21:00:52 +0800

    fix status code

--- zf/Response.php
+++ zf/Response.php
@@ -5,6 +5,45 @@ namespace zf;
 trait Response
 { 
 	private $debug;
+	private $statusCodes = [
+		200 => 'OK',
+		201 => 'Created',
+		202 => 'Accepted',
+		203 => 'Non-Authoritative Information',
+		204 => 'No Content',
+		205 => 'Reset Content',
+		206 => 'Partial Content',
+		300 => 'Multiple Choices',
+		301 => 'Moved Permanently',
+		302 => 'Found',
+		304 => 'Not Modified',
+		305 => 'Use Proxy',
+		307 => 'Temporary Redirect',
+		400 => 'Bad Request',
+		401 => 'Unauthorized',
+		403 => 'Forbidden',
+		404 => 'Not Found',
+		405 => 'Method Not Allowed',
+		406 => 'Not Acceptable',
+		407 => 'Proxy Authentication Required',
+		408 => 'Request Timeout',
+		409 => 'Conflict',
+		410 => 'Gone',
+		411 => 'Length Required',
+		412 => 'Precondition Failed',
+		413 => 'Request Entity Too Large',
+		414 => 'Request-URI Too Long',
+		415 => 'Unsupported Media Type',
+		416 => 'Requested Range Not Satisfiable',
+		417 => 'Expectation Failed',
+		500 => 'Internal Server Error',
+		501 => 'Not Implemented',
+		502 => 'Bad Gateway',
+		503 => 'Service Unavailable',
+		504 => 'Gateway Timeout',
+		505 => 'HTTP Version Not Supported'
+	];
+
 	public function lastModified($time)
 	{
 		header('Last-Modified: '. gmdate(DATE_RFC2822, $time));
@@ -50,7 +89,8 @@ trait Response
 
 	public function response($response)
 	{
-		header('HTTP/1.1: '. $response['code']);
+		global $statusCodes;
+		header('HTTP/1.1 '. $response['code'] . ' ' . $this->statusCodes[$response['code']]);
 		header('Status: '. $response['code']);
 		header('Content-Type: '. $response['type']);
 		if($this->config->debug)
