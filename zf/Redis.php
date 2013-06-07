76be3498969ea75a80bfdeabe50b6e2de585af43
Feng Zhou <zf.pascal@gmail.com>
2013-06-07 22:28:21 +0800

    throw exception if config not found

--- zf/Redis.php
+++ zf/Redis.php
@@ -5,46 +5,33 @@ namespace zf;
 class Redis
 {
 
-	private $config;
-	private $cachedConnections;
-	private $cachedClients;
+	private $_config;
+	private $_cachedConnections = [];
 
 	public function __construct($config)
 	{
-		$this->cachedConnections = array();
-		$this->cachedClients = array();
-		$this->config = $config;
+		$this->_config = $config;
 	}
 
 	public function __get($name)
 	{
-		if(empty($this->cachedClients[$name]))
-		{
-			if(isset($this->config[$name]))
-			{
-				$config = $this->config[$name];
-				$host = isset($config['host']) ? $config['host'] : '127.0.0.1';
-				$port = isset($config['port']) ? $config['port'] : 6379;
-				$timeout = isset($config['timeout']) ? $config['timeout'] : 5;
-				$pconnect = isset($config['pconnect']) ? $config['pconnect'] : false;
+		if(empty($this->_config[$name]))
+			throw new \Exception("redis \"$name\" not defined");
 
-				if (empty($this->cachedConnections["$host:$port"]))
-				{
-					$redis = new \Redis();
-					if ($pconnect)
-					{
-						$redis->pconnect($host, $port, $timeout);
-					}
-					else
-					{
-						$redis->connect($host, $port, $timeout);
-					}
-					$this->cachedConnections["$host:$port"] = $redis;
-				}
-				$conenction = $this->cachedConnections["$host:$port"];
-				$this->cachedClients[$name] = $conenction;
-			}
+		$config = $this->_config[$name];
+		$host = isset($config['host']) ? $config['host'] : '127.0.0.1';
+		$port = isset($config['port']) ? $config['port'] : 6379;
+		$timeout = isset($config['timeout']) ? $config['timeout'] : 0;
+		$pconnect = isset($config['pconnect']) ? $config['pconnect'] : false;
+
+		if(empty($this->_cachedConnections["$host:$port"]))
+		{
+			$redis = new \Redis();
+			$pconnect
+				? $redis->pconnect($host, $port, $timeout)
+				: $redis->connect($host, $port, $timeout);
+			$this->_cachedConnections["$host:$port"] = $redis;
 		}
-		return $this->cachedClients[$name];
+		return $this->$name = $this->_cachedConnections["$host:$port"];
 	}
 }
