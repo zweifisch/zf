c60fc479986f62e9b1b78f9356f34297fc4d73fb
Feng Zhou <zf.pascal@gmail.com>
2013-06-15 19:58:14 +0800

    pdo rewrite

--- zf/PDO.php
+++ zf/PDO.php
@@ -4,57 +4,117 @@ namespace zf;
 
 class PDO extends \PDO
 {
+	private $_queries;
+	private $_querie;
+	private $_params;
+
 	public function __construct($config)
 	{
 		parent::__construct($config['dsn'], $config['username'], $config['password']);
+		if(isset($config['queries']))
+		{
+			$this->_queries = $config['queries'];
+		}
 	}
 
-	public function fetchAll($query, $parameters=[], $fn=null)
+	public function fetchAll($fn=null)
 	{
-		$statement = $this->execute($query, $parameters);
-		$ret = $fn
-			? $statement->fetchAll(\PDO::FETCH_FUNC, $fn)
-			: $statement->fetchAll(\PDO::FETCH_ASSOC);
-		$statement->closeCursor();
-		return $ret;
+		return $this->execute(function($statement) use ($fn){
+			return $fn
+				? $statement->fetchAll(\PDO::FETCH_FUNC, $fn)
+				: $statement->fetchAll(\PDO::FETCH_ASSOC);
+		});
 	}
 
-	public function fetchOne($query, $parameters=[], $fn=null)
+	public function fetchOne($fn=null)
 	{
-		$statement = $this->execute($query, $parameters);
-		$ret = $fn
-			? $statement->fetch(\PDO::FETCH_FUNC, $fn)
-			: $statement->fetch(\PDO::FETCH_ASSOC);
-		$statement->closeCursor();
-		return $ret;
+		return $this->execute(function($statement) use ($fn){
+			return $fn
+				? $statement->fetch(\PDO::FETCH_FUNC, $fn)
+				: $statement->fetch(\PDO::FETCH_ASSOC);
+		});
 	}
 
-	public function fetchColumn($query, $parameters=[], $column=0)
+	public function fetchColumn($column=null)
 	{
-		$statement = $this->execute($query, $parameters);
-		$ret = $statement->fetchColumn($column);
-		$statement->closeCursor();
-		return $ret;
+		return $this->execute(function($statement) use ($column){
+			return $statement->fetchColumn((int)$column);
+		});
 	}
 
-	public function fetch($query, $params=[])
+	public function update()
 	{
-		$statement= $this->prepare($query);
-		$statement->setFetchMode(PDO::FETCH_ASSOC);
-		$statement->execute($params);
-		return $statement;
+		return $this->execute(function($statement){
+			return $statement->rowCount();
+		});
 	}
 
-	public function update($query, $parameters)
+	public function bind($params)
 	{
-		$statement = $this->execute($query, $parameters);
-		return $statement->rowCount();
+		foreach($params as $name=>$value)
+		{
+			$this->_params[] = [$name,$value,$this->getType($value)];
+		}
+		return $this;
 	}
 
-	private function execute($query, $parameters=[])
+	private function getType($val)
 	{
-		$statement = $this->prepare($query);
-		$statement->execute($parameters);
-		return $statement;
+		if(is_int($val)) return \PDO::PARAM_INT;
+		if(is_bool($val)) return \PDO::PARAM_BOOL;
+		if(is_null($val)) return \PDO::PARAM_NULL;
+		return \PDO::PARAM_STR;
+	}
+
+	public function getLastError()
+	{
+		return $this->_statement ? $this->_statement->errorInfo() : null;
+	}
+
+	public function dumpParams()
+	{
+		$this->_statement->debugDumpParams();
+	}
+
+	public function __call($name,$args)
+	{
+		if(!strncmp('bind', $name, 4))
+		{
+			$type = '\PDO::PARAM_'.strtoupper(substr($name,4));
+			if(!defined($type))
+			{
+				throw new \Exception("type \"$type\" not defined");
+			}
+			$this->_params[] = [$args[0], $args[1], constant($type)];
+			return $this;
+		}
+	}
+
+	public function __get($queryname)
+	{
+		if(empty($this->_queries[$queryname]))
+		{
+			throw new \Exception("query \"$queryname\" not defined");
+		}
+		$this->_query = $this->_queries[$queryname];
+		return $this;
+	}
+
+	private function execute($fetch)
+	{
+		$this->_statement = $this->prepare($this->_query);
+		if($this->_params){
+			foreach($this->_params as $param)
+			{
+				list($name, $value, $type) = $param;
+				$this->_statement->bindValue(':'.$name, $value, $type);
+			}
+			$this->_params = null;
+		}
+		$this->_statement->execute();
+		$result = $fetch($this->_statement);
+		$this->_query = null;
+		$this->_statement->closeCursor();
+		return $result;
 	}
 }
