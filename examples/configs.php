cd821dac9a097cfeebdfbaf1ac767b2943251087
Feng Zhou <zf.pascal@gmail.com>
2013-06-17 23:31:55 +0800

    AMQP

--- examples/configs.php
+++ examples/configs.php
@@ -7,12 +7,20 @@ $exports['mongo'] = [
 		'url'        => 'mongodb://localhost:27017',
 		'database'   => 'project',
 		'options'    => ['readPreference' => MongoClient::RP_SECONDARY_PREFERRED],
-	]];
+	]
+];
 
 $exports['redis'] = [
 	'default' => [
 		'host'     => 'localhost',
 		'pconnect' => true,
-	]];
+	]
+];
+
+$exports['mq'] = [
+	'host'     => 'localhost',
+	'login'    => 'guest',
+	'password' => 'guest',
+];
 
 return $exports;
